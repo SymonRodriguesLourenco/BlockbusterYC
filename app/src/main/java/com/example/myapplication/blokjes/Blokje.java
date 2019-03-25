fcdf113bf195d3bd5a43dd735b531a3a4d2aee79
rickn <nanningarick@gmail.com>
2019-03-25 16:38:36 +0100

    yo test

--- app/src/main/java/com/example/myapplication/blokjes/Blokje.java
+++ app/src/main/java/com/example/myapplication/blokjes/Blokje.java
@@ -99,11 +99,27 @@ public class Blokje extends Activity {
                 fromRight = true;
             }
         }
-        if (ballY + height + Math.abs(speedY) > maxY && ballY + height < maxY && ballX + width > minX && ballX < minX) {
-            fromUp = true;
+        if (ballY + height + Math.abs(speedY) > maxY) {
+            if (ballX + width > minX && ballX < minX){
+                fromUp = true;
+            }
+            else if (ballX + width + speedX > minX && goingRight){
+                fromUp = true;
+            }
+            else if (ballX - speedX < maxX && !goingRight){
+                fromUp = true;
+            }
         }
-        else if (ballY - Math.abs(speedY) < minY && ballY > minY && ballX + width > minX && ballX < minX) {
-            fromDown = true;
+        else if (ballY - Math.abs(speedY) < minY) {
+            if (ballX + width > minX && ballX < minX){
+                fromDown = true;
+            }
+            else if (ballX + width + speedX > minX && goingRight){
+                fromDown = true;
+            }
+            else if (ballX - speedX < maxX && !goingRight){
+                fromDown = true;
+            }
         }
     }
 
