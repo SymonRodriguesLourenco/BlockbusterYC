53727b92ab4e6591cb086cc5beaa28ff8b5e38fe
alanobr <a.braam@youngcapitalacademy.nl>
2019-04-04 10:45:58 +0200

    hoi

--- app/src/main/java/com/example/myapplication/blokjes/Powerupblock.java
+++ app/src/main/java/com/example/myapplication/blokjes/Powerupblock.java
@@ -18,7 +18,7 @@ public class Powerupblock extends Block{
     public Powerupblock(int X, int Y, int width, int height, String powerup, Resources resources) {
         super(X, Y, width, height,  1);
         this.powerup = powerup;
-        this.img = BitmapFactory.decodeResource(resources, R.drawable.tomaat);
+        this.img = BitmapFactory.decodeResource(resources, R.drawable.popcorn);
         this.img = Bitmap.createScaledBitmap(this.img, this.height, this.height, false);
     }
 
